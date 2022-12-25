cd2281c31ce23c38537b9a61954434d939b764fb
Andrew Byerle <andran@live.unc.edu>
2022-12-25 00:49:39 -0500

    Initial Commit

---
+++ ChatAppFirebase/ChatAppFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppFirebaseApp.swift
+//  ChatAppFirebase
+//
+//  Created by Andrew Byerle on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
