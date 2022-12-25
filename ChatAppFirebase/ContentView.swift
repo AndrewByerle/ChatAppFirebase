81251ce33a06e0970776f9067800e52ef0a8ff7e
Andrew Byerle <andran@live.unc.edu>
2022-12-25 01:37:01 -0500

    Create Acct

--- ChatAppFirebase/ContentView.swift
+++ ChatAppFirebase/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoginShown = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ScrollView{
+                Picker("AuthType", selection: $isLoginShown) {
+                    Text("Login").tag(false)
+                    Text("Create Account").tag(true)
+                }.pickerStyle(SegmentedPickerStyle())
+                 .padding()
+                Button {
+                    print("")
+                } label: {
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 64))
+                        .padding()
+                }
+                TextField("Email", text: $email)
+                TextField("Password", text: $password)
+                Button {
+                } label: {
+                    HStack{
+                        Spacer()
+                        Text("Create Acct")
+                            .foregroundColor(.white)
+                            .padding(.vertical, 10)
+                        Spacer()
+                    }.background(Color.blue)
+                }
+
+            }.navigationTitle("Create Account")
         }
-        .padding()
     }
 }
 
