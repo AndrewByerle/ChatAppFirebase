4ced9595f982e31c0f4e22c9cc04af70d51427ed
Andrew Byerle <andran@live.unc.edu>
2022-12-30 02:24:02 -0500

    firebase storage

--- ChatAppFirebase/AuthView.swift
+++ ChatAppFirebase/AuthView.swift
@@ -6,11 +6,17 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseStorage
+
+
 
 struct AuthView: View {
     @State var isLoginShown = false
     @State var email = ""
     @State var password = ""
+    @State var loginStatus = ""
+    @State var shouldShowImagePicker = false
     
     var body: some View {
         NavigationView{
@@ -22,12 +28,22 @@ struct AuthView: View {
                     }.pickerStyle(SegmentedPickerStyle())
                     if !isLoginShown {
                         Button {
-                            print("")
+                            shouldShowImagePicker.toggle()
                         } label: {
-                            Image(systemName: "person.fill")
-                                .font(.system(size: 64))
-                                .padding()
-                        }
+                            VStack{
+                                if let image = self.image{
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .frame(width: 120, height: 120)
+                                        .scaledToFit()
+                                        .cornerRadius(64)
+                                } else{
+                                    Image(systemName: "person.fill")
+                                        .font(.system(size: 64))
+                                        .padding()
+                                }
+                            }
+                        }.overlay(RoundedRectangle(cornerRadius: 64).stroke(Color.blue, lineWidth: 3))
                     }
                     Group{
                         TextField("Email", text: $email)
@@ -48,16 +64,71 @@ struct AuthView: View {
                             Spacer()
                         }.background(Color.blue)
                     }
+                    Text(loginStatus).foregroundColor(Color.red)
                 }.padding()
             }.navigationTitle(isLoginShown ? "Log in" : "Create Account")
                 .background(Color(.init(white: 0, alpha: 0.06)).ignoresSafeArea())
-        }
+        }.navigationViewStyle(StackNavigationViewStyle())
+            .fullScreenCover(isPresented: $shouldShowImagePicker){
+                ImagePicker(image: $image)
+            }
     }
+    
+    @State var image: UIImage?
+    
     private func handleAction() {
         if isLoginShown{
-            print("Log in user")
+            loginUser()
         } else{
-            print("Create account, store user in firestore and image in storage")
+            createUser()
+        }
+    }
+    
+    private func loginUser(){
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.loginStatus = "Failed to login user: \(error)"
+                return
+            }
+            print("logged in \(authResult?.user.uid ?? "")")
+            loginStatus = "logged in \(authResult?.user.uid ?? "")"
+        }
+    }
+    
+    
+    private func createUser(){
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error{
+                self.loginStatus = "Failed to create user: \(error)"
+                return
+            }
+            print("Created user \(authResult?.user.uid ?? "")")
+            loginStatus = "created account for \(authResult?.user.uid ?? "")"
+            
+            self.persistImageToStorage()
+            
+        }
+    }
+    
+    private func persistImageToStorage(){
+        guard let uid = Auth.auth().currentUser?.uid else
+            { return }
+        let ref = Storage.storage().reference(withPath: uid)
+        guard let data = self.image?.jpegData(compressionQuality: 0.5) else
+            { return }
+        ref.putData(data) { metaData, error in
+            if let error = error{
+                self.loginStatus = "failed to upload img to storage: \(error)"
+                return
+            }
+            
+            ref.downloadURL { url, error in
+                if let error = error{
+                    self.loginStatus = "failed to retrieve downloadUrl: \(error)"
+                    return
+                }
+                self.loginStatus = "Successfully downloaded url: \(url?.absoluteString ?? "")"
+            }
         }
     }
 }
